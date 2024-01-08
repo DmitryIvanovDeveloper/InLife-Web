be25f462332cec5684e367ed232eca8597139192
Dmitry Ivanov <dmitry.ivanov.developer@gmail.com>
2024-01-08 19:30:37 +0400

    Removed build folder

--- src/components/buttons/TextButton.tsx
+++ src/components/buttons/TextButton.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Button } from "@mui/material";
 
 interface ITextButtonProps {
-  children: any;
-  onClick: (event: any) => void;
+    children: any;
+    onClick: (event: any) => void;
 }
 
 export default function TextButton(props: ITextButtonProps) {
-  return <Button
-    disableRipple
-    variant="text"
-    onClick={props.onClick}
-    sx={{
-      display:"flex",
-      justifyContent:"flex-start",
-      textAlign: "left"
-    }}>
-    {props.children}
-  </Button>
+    return <Button
+        disableRipple
+        variant="text"
+        onClick={props.onClick}
+        sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            textAlign: "left"
+        }}>
+        {props.children}
+    </Button>
 }
